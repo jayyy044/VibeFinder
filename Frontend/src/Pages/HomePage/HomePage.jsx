436a77fcfcbf04b61a102d71a070124b21b635dd
Maanas <maanasksaxena@gmail.com>
2024-11-09 21:01:15 -0700

    desgining Frontend

--- Frontend/src/Pages/HomePage/HomePage.jsx
+++ Frontend/src/Pages/HomePage/HomePage.jsx
@@ -6,9 +6,16 @@ const HomePage = () => {
         <>
             <div className="HomePageContainer">
                 <div className="TextContainer">
-
+                    <div className="Welcome">
+                        <h1>Certifed Compiler Boys Bring You:</h1>
+                        <h1 className="CompanyName">Vybe-Fynd3r</h1>
+                    </div>
+                    <div class="CatchPhrase">
+                        Envisioning Your Vibe
+                    </div>
                 </div>
                 <div className="ImageContainer">
+                    <h1 className="image">HEllllooo</h1>
                     
                 </div>
             </div>
