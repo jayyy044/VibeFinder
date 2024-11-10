c9adbc58e52fdc9b8a4925623163c3c984f9865f
Maanas <maanasksaxena@gmail.com>
2024-11-10 01:21:54 -0700

    react app done

--- GestureModel/imageapp.py
+++ GestureModel/imageapp.py
@@ -1,7 +1,14 @@
 from flask import Flask, send_file
+# from flask import Flask, jsonify
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/image', methods=['GET'])
 def send_image():
     return send_file('./Final.jpg', mimetype='image/jpeg')
 
+
+
+# @app.route('/image', methods=['GET'])
+# def send_image():
+#     image_url = './Final.jpg'  # Change this to the actual path
+#     return jsonify({"image_url": image_url})
