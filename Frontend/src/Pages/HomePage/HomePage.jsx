80316fcba1580dac0f642f97bc0cf1ccc624f9b1
Maanas <maanasksaxena@gmail.com>
2024-11-10 11:10:28 -0700

    final commit

--- Frontend/src/Pages/HomePage/HomePage.jsx
+++ Frontend/src/Pages/HomePage/HomePage.jsx
@@ -3,15 +3,18 @@ import './HomePage.css';
 import LogoPng from '../../assets/logo.png'
 
 const HomePage = () => {
+    const [Page, setPage] = useState(false)
+    const [apiResonse, setapiResponse] = useState()
     const GrabImage = async () => {
         try{
-            const response = await fetch('/api/image');
-            const blob = await response.blob();  
+            const response = await fetch('/api');
+            const data = await response.json();  
             if(!response.ok){
                 console.log("There was an error recieving image")
             }
-            const url = URL.createObjectURL(blob);
-            console.log("Url Recieved", url)
+            console.log("Music Data", data)
+            setapiResponse(data)
+            setPage(true)
 
         }
         catch(error){
@@ -23,22 +26,38 @@ const HomePage = () => {
     }
     return(
         <>
-            <div className="HomePageContainer">
-                <div className="TextContainer">
-                    <div className="Welcome">
-                        <p>Certifed Compiler Boys Bring You:</p>
-                        <p className="CompanyName">Vybe-Fynd3r</p>
+            {!Page && (
+                <div className="HomePageContainer">
+                    <div className="TextContainer">
+                        <div className="Welcome">
+                            <p>Certified Compiler Boys Bring You:</p>
+                            <p className="CompanyName">Vybe-Fynd3r</p>
+                        </div>
+                        <div className="CatchPhrase">
+                            Vision To Vibes
+                        </div>
                     </div>
-                    <div class="CatchPhrase">
-                        Vision To Vibes
+                    <div className="ImageContainer">
+                        <div className="ImgCont" onClick={GrabImage}>
+                            <img src={LogoPng} alt="Logo" />
+                        </div>
                     </div>
                 </div>
-                <div className="ImageContainer">
-                    <div className="ImgCont" onClick={GrabImage}>
-                        <img src={LogoPng}/>
-                    </div>
-                </div>
-            </div>
+            )}
+
+            {Page && 
+                 <div className="DataContainer">
+                    {apiResonse.map((track, index) => (
+                        <div key={index} className="TrackCard">
+                            <img src={track[2]} alt={`${track[1]} cover`} /> {/* Image */}
+                            <h1>{track[1]}</h1>
+                            <h1>{`Artist: ${track[0]}`}</h1> {/* Artist Name */}
+                             {/* Track Title */}
+                            
+                        </div>
+                    ))}
+                 </div>
+            }
         </>
     )
 
