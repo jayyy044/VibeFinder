2ed1bfe56cdeba2f8e3854022a04cc8fe0073ef9
Maanas <maanasksaxena@gmail.com>
2024-11-09 19:30:44 -0700

    Got the frontend working

---
+++ Frontend/src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './HomePage.css';
+
+const HomePage = () => {
+    return(
+        <>
+            <div className="HomePageContainer">
+                <div className="TextContainer">
+
+                </div>
+                <div className="ImageContainer">
+                    
+                </div>
+            </div>
+        </>
+    )
+
+}
+
+export default HomePage;
